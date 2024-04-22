09901a4d5219a4745778051ce7d356c3b9fb491a
Sunniva Sørensen <susor6866@oslomet.no>
2024-04-22 14:03:26 +0200

    Java filer

--- src/main/java/com/example/oblig3/TicketController.java
+++ src/main/java/com/example/oblig3/TicketController.java
@@ -1,5 +1,29 @@
 package com.example.oblig3;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+
 public class TicketController {
-    @
-}
+    private final List<Ticket> tickets = new ArrayList<>();
+
+    @PostMapping ("/saveTicket")
+    public void saveTicket (Ticket inTicket) {
+        tickets.add(inTicket);
+    }
+
+    @GetMapping ("/getMovie")
+    public List<Movie> movies() {
+        List<Movie> newMovie = new ArrayList<>();
+        newMovie.add(new Movie("Back To Black"));
+        newMovie.add(new Movie("One Life"));
+        newMovie.add(new Movie("Ibelin"));
+        return newMovie;
+    }
+
+    }
