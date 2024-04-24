8a884a7378fb8a205a12d5f5ee833309a604cd1b
Sunniva Sørensen <susor6866@oslomet.no>
2024-04-24 14:53:08 +0200

    Ferdig js

--- src/main/resources/static/changes.js
+++ src/main/resources/static/changes.js
@@ -1,4 +1,27 @@
 $(function () {
     const id = window.location.search.substring(1);
-    const
-})
+    const url = "/getOne?" + id;
+    $.get(url, function (ticket) {
+        $("#id").val(ticket.id);
+        $("#movie").val(ticket.movie);
+        $("#number").val(ticket.number);
+        $("#firstname").val(ticket.firstname);
+        $("#lastname").val(ticket.lastname);
+        $("#phoneNb").val(ticket.phoneNb);
+        $("#email").val(ticket.email);
+    });
+});
+
+function changeTicket() {
+    const ticket = {
+        movie: $("#chosenMovie").val(),
+        number: $("#number").val(),
+        firstname: $("#firstname").val(),
+        lastname: $("#lastname").val(),
+        phoneNb: $("#phoneNb").val(),
+        email: $("#email").val()
+    };
+    $.post("/changeTicket", ticket, function () {
+        window.location.href="/index.html";
+    });
+}
